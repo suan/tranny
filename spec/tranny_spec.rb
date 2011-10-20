624a80bcd2174f4244bd0c26757c2c05be5d9ac7
Josh Krueger <jkrueger@enovafinancial.com>
2011-10-20 11:54:58 -0500

    Added MOAR tests. My wrist hurts.

--- spec/tranny_spec.rb
+++ spec/tranny_spec.rb
@@ -56,7 +56,7 @@ describe Tranny do
     end
     
     context "the value should be capitalized" do
-       it "should produce the same key and the value should be capitalized" do
+       it "should produce the same key" do
         class TestTranny < Tranny
           transform do
             input "foo" => "foo", :via => :capitalize
@@ -69,7 +69,130 @@ describe Tranny do
         TestTranny.convert(input_hash).should == desired_hash
       end
 
+      it "should produce a different key" do
+        class TestTranny < Tranny
+          transform do
+            input "foo" => :foo, :via => lambda { |x| x.capitalize }
+          end
+        end
+
+        input_hash = { "foo" => "bar" }
+        desired_hash = { :foo => "Bar" }
+
+        TestTranny.convert(input_hash).should == desired_hash
+      end
       
     end
+
+    context "use nested input" do
+      it "should produce the same key without nesting" do
+        class TestTranny < Tranny
+          transform do
+            input ["foo", "bar"] => "bar"
+          end
+        end
+
+        input_hash = { "foo" => { "bar" => "FOOBAR!" } }
+        desired_hash = { "bar" => "FOOBAR!" }
+
+        TestTranny.convert(input_hash).should == desired_hash
+      end
+
+      it "should produce a different key without nesting" do
+        class TestTranny < Tranny
+          transform do
+            input ["foo", "bar"] => :foo_bar
+          end
+        end
+
+        input_hash = { "foo" => { "bar" => "FOOBAR!" } }
+        desired_hash = { :foo_bar => "FOOBAR!" }
+
+        TestTranny.convert(input_hash).should == desired_hash
+      end
+    end
+
+    context "use nested output" do
+      it "should produce the same key, but nested" do
+        class TestTranny < Tranny
+          transform do
+            input :foo => [:bar, :foo]
+          end
+        end
+
+        input_hash = { :foo => "FOO!" }
+        desired_hash = { :bar => { :foo => "FOO!" } }
+
+        TestTranny.convert(input_hash).should == desired_hash
+      end
+
+      it "should produce a different key, but nested" do
+        class TestTranny < Tranny
+          transform do
+            input "foo" => [:bar, :foo]
+          end
+        end
+      
+        input_hash = { "foo" => "FOO!" }
+        desired_hash = { :bar => { :foo => "FOO!" } }
+
+        TestTranny.convert(input_hash).should == desired_hash
+      end
+
+      it "should use nested input and produce the same key, but nested differently" do
+        class TestTranny < Tranny
+          transform do
+            input ["foo", "bar"] => ["baz", "bar"]
+          end
+        end
+
+        input_hash = { "foo" => { "bar" => "BAR!" } }
+        desired_hash = { "baz" => { "bar" => "BAR!" } }
+
+        TestTranny.convert(input_hash).should == desired_hash
+      end
+
+      it "should use nested input and produce a different key, but nested differently" do
+        class TestTranny < Tranny
+          transform do
+            input ["foo", "bar"] => [:baz, :bar]
+          end
+        end
+
+        input_hash = { "foo" => { "bar" => "BAR!" } }
+        desired_hash = { :baz => { :bar => "BAR!" } }
+
+        TestTranny.convert(input_hash).should == desired_hash
+      end
+    end
+
+    context "use multiple inputs" do
+      it "should join two elements with a space and produce a single key" do
+        class TestTranny < Tranny
+          transform do
+            input_multiple [:foo, :bar] => :foo_bar
+          end
+        end
+
+        input_hash = { :foo => "Foo", :bar => "Bar" }
+        desired_hash = { :foo_bar => "Foo Bar" }
+
+        TestTranny.convert(input_hash).should == desired_hash
+      end
+
+      it "should join two nested elements with a dash, via a lambda and produce a single key" do
+        class TestTranny < Tranny
+          transform do
+            input_multiple [[:foo, :bar], [:baz, :qux]] => :bar_qux, :via => lambda { |x| x.join("-") }
+          end
+        end
+
+        input_hash = { :foo => { :bar => "fbar" }, :baz => { :qux => "bqux" } }
+        desired_hash = { :bar_qux => "fbar-bqux" }
+
+        TestTranny.convert(input_hash).should == desired_hash
+      end
+    end
+
   end
 end
